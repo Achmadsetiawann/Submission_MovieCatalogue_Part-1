373a75122f6da740fb8e163cfca8dc8aac1e5a35
Achmadsetiawan <achmadsetiawannn@gmail.com>
2019-08-28 00:28:57 +0700

    Adding Adapter ListView_1

--- app/src/main/java/com/example/submissionmoviecatalogue/MainActivity.java
+++ app/src/main/java/com/example/submissionmoviecatalogue/MainActivity.java
@@ -9,11 +9,14 @@ import android.widget.ListView;
 public class MainActivity extends AppCompatActivity {
 
 /*
+Structure MainActivity =>  activity_main.xml
 
 Structure MainActivity =>  activity_main.xml
 
+
 */
 
+    /* Initials Data */
     private String[] dataName = {"Cut Nyak Dien","Ki Hajar Dewantara","Moh Yamin","Patimura","R A Kartini","Sukarno"};
 
     @Override
@@ -21,6 +24,7 @@ Structure MainActivity =>  activity_main.xml
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /* Adapter Function Call Back ArrayData */
         ListView listView = findViewById(R.id.lv_list_movie);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, dataName);
         listView.setAdapter(adapter);
